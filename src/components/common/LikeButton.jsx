cf5ba3a73fb4abbaa85dfa5977b5a9472ee1c093
Harry Ward-Gray <wardgray24@gmail.com>
2023-09-05 12:26:04 +0100

    create error message for like button component

--- src/components/common/LikeButton.jsx
+++ src/components/common/LikeButton.jsx
@@ -8,33 +8,43 @@ export default function LikeButton({
   targetId,
 }) {
   const [selected, setSelected] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   const modifier = like ? 1 : -1;
 
   return (
-    <button
-      onClick={() => {
-        if (!selected) {
-          const amount = 1 * modifier;
-          setSelected(true);
-          updateVotes(amount);
-          patchTarget(targetId, amount).catch((err) => {
-            setSelected(false);
-            updateVotes(amount * -1);
-          });
-        } else {
-          const amount = -1 * modifier;
-          setSelected(false);
-          updateVotes(amount);
-          patchTarget(targetId, amount).catch((err) => {
+    <>
+      <button
+        onClick={() => {
+          if (!selected) {
+            setIsError(false);
+            const amount = 1 * modifier;
             setSelected(true);
-            updateVotes(amount * -1);
-          });
+            updateVotes(amount);
+            patchTarget(targetId, amount).catch((err) => {
+              setIsError(true);
+              setSelected(false);
+              updateVotes(amount * -1);
+            });
+          } else {
+            const amount = -1 * modifier;
+            setIsError(false);
+            setSelected(false);
+            updateVotes(amount);
+            patchTarget(targetId, amount).catch((err) => {
+              setIsError(true);
+              setSelected(true);
+              updateVotes(amount * -1);
+            });
+          }
+        }}
+        className={
+          selected ? "interact-button user-selected" : "interact-button"
         }
-      }}
-      className={selected ? "interact-button user-selected" : "interact-button"}
-    >
-      <img src={likeImg} className={like ? "icon" : "icon dislike"} />
-    </button>
+      >
+        <img src={likeImg} className={like ? "icon" : "icon dislike"} />
+      </button>
+      {isError ? <p className="error-msg">Something went wrong</p> : <></>}
+    </>
   );
 }
