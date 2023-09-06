15af7be794b2e2fcaea1c3961291e3daa2e0756a
Harry Ward-Gray <wardgray24@gmail.com>
2023-09-06 13:16:57 +0100

    implement static user profile section, update styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { UserContext } from "./contexts/User";
+
 import Header from "./components/Header";
 import HomePage from "./components/HomePage";
 import ArticlePage from "./components/ArticlePage";
 import TopicsPage from "./components/TopicsPage";
 import TopicPage from "./components/TopicPage";
+import ProfilePicture from "./components/common/ProfilePicture";
 import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
+  const { username } = useContext(UserContext);
+
   return (
     <>
       <Header />
@@ -18,30 +23,42 @@ function App() {
           <Route path="/topics/:topic" element={<TopicPage />} />
           <Route path="/topics/:topic/:article_id" element={<ArticlePage />} />
         </Routes>
-        <div id="menu-wrap">
-          <Link
-            className="menu-link"
-            to="/"
-            onClick={() => window.scrollTo(0, 0)}
-          >
-            Home
-          </Link>
-          <Link to="/topics" className="menu-link">
-            Topics
-          </Link>
-          <Link className="menu-link" id="create-post-link">
-            Create Post
-          </Link>
-          <footer>
-            <a>Footer link</a>
-            <a>Footer link</a>
-            <a>Footer link</a>
-            <a>Footer link</a>
-            <p>
-              NC News, Inc. © 2023.
-              <br /> All rights reserved.
-            </p>
-          </footer>
+        <div className="sidebar">
+          <div className="user-wrap">
+            <Link id="log-out" className="menu-link">
+              Log Out
+            </Link>
+            <div id="username-picture-wrap">
+              <p>{username}</p>
+              <ProfilePicture author={username} />
+            </div>
+          </div>
+
+          <div id="menu-wrap">
+            <Link
+              className="menu-link"
+              to="/"
+              onClick={() => window.scrollTo(0, 0)}
+            >
+              Home
+            </Link>
+            <Link to="/topics" className="menu-link">
+              Topics
+            </Link>
+            <Link className="menu-link" id="create-post-link">
+              Create Post
+            </Link>
+            <footer>
+              <a>Footer link</a>
+              <a>Footer link</a>
+              <a>Footer link</a>
+              <a>Footer link</a>
+              <p>
+                NC News, Inc. © 2023.
+                <br /> All rights reserved.
+              </p>
+            </footer>
+          </div>
         </div>
       </div>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,19 @@
+import { useContext } from "react";
+import { UserContext } from "../contexts/User";
+import ProfilePicture from "./common/ProfilePicture";
+import { Link } from "react-router-dom";
 export default function Header() {
+  const { username } = useContext(UserContext);
   return (
     <header>
-      <h1>NC News</h1>
+      <div className="header_title-nav-wrap">
+        <h1>NC News</h1>
+        <nav className="header_nav">
+          <Link to="/">Home</Link>
+          <Link to="/topics">Topics</Link>
+          <Link to="/">Create Post</Link>
+        </nav>
+      </div>{" "}
     </header>
   );
 }
